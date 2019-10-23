811c7acd337c2077df49f9bc0b089059d657c6e8
Kexin Hong <ruby@berkeley.edu>
2019-10-23 08:18:49 +0000

    tests

--- test/test_DictionaryTrie.cpp
+++ test/test_DictionaryTrie.cpp
@@ -34,19 +34,23 @@ TEST(DictTrieTests, INSERT_FIND_TEST) {
 /* PredictCompletions test. */
 TEST(DictTrieTests, PREDICT_COMPLETIONS_TEST) {
     DictionaryTrie dict;
-    ASSERT_EQ(dict.insert("app", 2), true);
-    ASSERT_EQ(dict.insert("apple", 3), true);
+    ASSERT_TRUE(dict.insert("app", 2));
+    ASSERT_FALSE(dict.insert("app", 2));
+    ASSERT_TRUE(dict.insert("apple", 3));
+    ASSERT_FALSE(dict.find("truth"));
+    ASSERT_TRUE(dict.insert("truth", 5));
     vector<string> vec1 = {"apple", "app"};
     vector<string> returnVec = dict.predictCompletions("ap", 10);
     ASSERT_EQ(returnVec, vec1);
 }
-TEST(DictTrieTests, MORE_DATA_TEST) {
-    DictionaryTrie dict;
-    ifstream in;
-    in.open("unique_freq_dict.txt", ios::binary);
-    ASSERT_TRUE(in.is_open());
-    in.seekg(0, ios_base::end);
-    unsigned int len = in.tellg();
-    ASSERT_NE(len, 0);
-    Utils::loadDict(dict, in);
-}
+
+// TEST(DictTrieTests, MORE_DATA_TEST) {
+//     DictionaryTrie dict;
+//     ifstream in;
+//     in.open("unique_freq_dict.txt", ios::binary);
+//     ASSERT_TRUE(in.is_open());
+//     in.seekg(0, ios_base::end);
+//     unsigned int len = in.tellg();
+//     ASSERT_NE(len, 0);
+//     Utils::loadDict(dict, in);
+// }
